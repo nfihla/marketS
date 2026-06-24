3aad24f7179239783db94ce35837b45a27efd43f
nfihla <nondumisofihla16@gmail.com>
2026-06-24 02:46:19 +0200

    Create auth_layout_bottom.php

---
+++ includes/auth_layout_bottom.php
@@ -0,0 +1,11 @@
+            </div>
+
+            <p class="text-center text-xs text-slate-400 mt-6">
+                Secured with PHP sessions &amp; MySQL password hashing
+            </p>
+        </div>
+    </main>
+
+    <script>lucide.createIcons();</script>
+</body>
+</html>
